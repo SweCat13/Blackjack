b13250db54b581af6536e6e5c369dc3423ad96a9
SweCat13 <claranermark@hotmail.com>
2015-04-20 15:02:13 +0200

    Console
    Spel-loop

---
+++ BlackJack/BlackJack/BlackJack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackJack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Play Black Jack");
+            Console.WriteLine("Start the game by dealing cards? (y/n)");
+            string strStartGame = Console.ReadLine();
+
+            while (strStartGame == "y")                             //spel-loop
+            {
+                int[] iArrCards = new int[10];
+                int[] iArrplayerCards = new int[10]; 
+
+                    arriMyArray[i] = r.Next(1, 10); 
+            
+            }
+
+            
+
+        }
+    }
+}
