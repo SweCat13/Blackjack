4ca3438a68ec8c9a439b05e708fbbc00df5d3cc5
SweCat13 <claranermark@hotmail.com>
2015-04-20 14:32:34 +0200

    Project commit
    First code commit

---
+++ BlackJack/BlackJack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackJack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Play Black Jack");
+            Console.WriteLine("Start the game by dealing cards? (y/n)");
+            string strStartGame = Console.ReadLine();
+
+            if (strStartGame = "y")
+            { }
+
+
+        }
+    }
+}
