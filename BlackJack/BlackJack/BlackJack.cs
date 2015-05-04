725452893f4646c011cc28007290e81ac6c925c7
SweCat13 <claranermark@hotmail.com>
2015-05-04 15:02:48 +0200

    Påbörjat summering av båda händer

--- BlackJack/BlackJack/BlackJack.cs
+++ BlackJack/BlackJack/BlackJack.cs
@@ -14,60 +14,72 @@ namespace BlackJack
             int[] iArrDealerCards = new int[10];
             int[] iArrPlayerCards = new int[10];
             Program prg = new Program();
+            int iSumHandOne = 0;
+            int iSumhandTwo = 0; 
 
             Console.WriteLine("Play Black Jack\n Start the game by dealing cards? (y/n)");
             string strStartGame = Console.ReadLine();
 
             if (strStartGame == "y")                                //spel-loop
             {
-               for (int i = 0; i < 2; i++)
-<<<<<<< HEAD
-               {
+                for (int i = 0; i < 2; i++)
+                {
+                    
+
+                     iArrDealerCards[i] = prg.AssignDealerCard(rDealer, iArrDealerCards[i]); //hämtar slumpat kort och berättar vilket kort som finns för dealer
+                    
+                        
+                    if(iArrDealerCards[i] == 11)
+                    {
+                        Console.WriteLine("Knekt");
+                    }
+                    else if (iArrDealerCards[i] == 12)
+                    {
+                        Console.WriteLine("Dam");
+                    }
+                    else if (iArrDealerCards[i] == 13)
+                    {
+                        Console.WriteLine("Kung");
+                    }
+                    else
+                    {
+                        Console.WriteLine(iArrDealerCards[i]);
+                    }
 
 
-=======
-               {  
->>>>>>> origin/master
-                   iArrDealerCards[i] = prg.AssignDealerCard(rDealer, iArrDealerCards[i]);
-                   Console.WriteLine(iArrDealerCards[i]);
-                   
+                    iArrPlayerCards[i] = prg.AssignPlayerCard(rDealer, iArrPlayerCards[i]); //hämtar slumpat kort och berättar vilket kort som finns för player
+                       
+                    if (iArrPlayerCards[i] == 11)
+                        {
+                            Console.WriteLine("Knekt");
+                        }
+                        else if (iArrPlayerCards[i] == 12)
+                        {
+                            Console.WriteLine("Dam");
+                        }
+                        else if (iArrPlayerCards[i] == 13)
+                        {
+                            Console.WriteLine("Kung");
+                        }
+                        else
+                        {
+                            Console.WriteLine(iArrPlayerCards[i]);
+                        }
+                  
+                    iSumHandOne = prg.CalcHands(iArrPlayerCards[i]);
 
-                   iArrPlayerCards[i] = prg.AssignPlayerCard(rDealer, iArrPlayerCards[i]);
-<<<<<<< HEAD
-                   if (iArrPlayerCards[i] == 11)
-                   {
-                       Console.WriteLine("Knekt");
-                   }
+                        
 
-                   else if (iArrPlayerCards[i] == 12)
-                   {
-                       Console.WriteLine("Dam");
-                   }
-                   else if (iArrPlayerCards[i] == 13)
-                   {
-                       Console.WriteLine("Kung");
-                   }
-                   else
-                   {
-                       Console.WriteLine(iArrPlayerCards[i]);
-                   }
+                    
+                }
 
-=======
-                   Console.WriteLine(iArrPlayerCards[i]);
->>>>>>> origin/master
-               }
-            }
-
-            int siffra = 0;
-            for (int r = 0; r < iArrPlayerCards.Length; r++ ) 
-            {
-                siffra = iArrDealerCards[r] + siffra; 
             }
         }
 
 
 
-        public int AssignDealerCard(Random rand, int iCard)
+         
+        public int AssignDealerCard(Random rand, int iCard) //slumpar fram ett random kort
         {
             iCard = rand.Next(1, 14);
             return iCard;
@@ -79,18 +91,15 @@ namespace BlackJack
             iCardP = randP.Next(1, 10);
             return iCardP; 
         }
-<<<<<<< HEAD
-=======
 
-         int Addition(int iPlayerCardOne, int iPlayerCardTwo)
+        public int CalcHands(int[] iInputSum) //summerar dealer eller player hand
         {
-                
+            return 0;
         }
->>>>>>> origin/master
            
 
-        }             
+    }             
           
        
-    }
+}
 
