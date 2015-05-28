ea127cb54c27c4d7f04743f16d56f4b575f3d2d6
SweCat13 <claranermark@hotmail.com>
2015-05-28 10:00:42 +0200

    KevinFIxat

--- BlackJack/BlackJack/BlackJack.cs
+++ BlackJack/BlackJack/BlackJack.cs
@@ -18,9 +18,13 @@ namespace BlackJack
             int[] iArrPlayerCards = new int[10];
             Program prg = new Program();
             int iSumHandOne = 0;
-            int iSumHandTwo = 0;
+            int iSumHandTwo = 0; 
+            int iBustD = 0;
+            int iBustP = 0;
+            bool bPlayerBust = false;
+            bool bDealerBust = false;
+            int iCon = 0;
 
-    
             string strPlayersName = string.Empty;
 
             Console.WriteLine("Enter Name ");
@@ -37,11 +41,12 @@ namespace BlackJack
 
             if (strStartGame == "y")                                //spel-loop
             Console.Clear();
+
+
             {
-                for (int i = 0; i < 2; i++)
+                for (int i = 0; i < 2 ; i++)
                 {
                     
-
                      iArrDealerCards[i] = prg.AssignDealerCard(rDealer, iArrDealerCards[i]); //anropar metod för att slumpa kort och berättar vilket kort som finns för dealer
                     
                         
@@ -66,40 +71,81 @@ namespace BlackJack
                         Console.WriteLine(iArrDealerCards[i]);
                     }
 
-
                     iArrPlayerCards[i] = prg.AssignPlayerCard(rDealer, iArrPlayerCards[i]); //anropar metod för att slumpa kort och berättar vilket kort som finns för player
-                       
+
                     if (iArrPlayerCards[i] == 11)
-                        {
-                            Console.WriteLine("Knekt");
-                        }
-                        else if (iArrPlayerCards[i] == 12)
-                        {
-                            Console.WriteLine("Dam");
-                        }
-                        else if (iArrPlayerCards[i] == 13)
-                        {
-                            Console.WriteLine("Kung");
-                        }
-                        else if (iArrPlayerCards[i] == 14)
-                        {
-                            Console.WriteLine("Ess");                        
-                        }
-                        else
-                        {
-                            Console.WriteLine(iArrPlayerCards[i]);
-                        }
+                    {
+                        Console.WriteLine("Knekt");
+                    }
+                    else if (iArrPlayerCards[i] == 12)
+                    {
+                        Console.WriteLine("Dam");
+                    }
+                    else if (iArrPlayerCards[i] == 13)
+                    {
+                        Console.WriteLine("Kung");
+                    }
+                    else if (iArrPlayerCards[i] == 14)
+                    {
+                        Console.WriteLine("Ess");
+                    }
+                    else
+                    {
+                        Console.WriteLine(iArrPlayerCards[i]);
+                    }
                   
                     
                 }
 
-                iSumHandOne = prg.CalcDhand(iArrDealerCards);   //Andropar metoden för att sumera dealerns hand
+
+                    iSumHandOne = prg.CalcDhand(iArrDealerCards);  //Andropar metoden för att sumera dealerns hand
                 Console.WriteLine("Dealer sum: " + iSumHandOne);
 
                 iSumHandTwo = prg.CalcPhand(iArrPlayerCards);   //Andropar metoden för att sumera players hand
                 Console.WriteLine("Player sum: " + iSumHandTwo);
 
-                prg.Bust(); //Kolar om dealern är busted
+                bDealerBust = prg.IsBusted(iSumHandOne);
+                Console.WriteLine(iBustD);
+
+                bPlayerBust = prg.IsBusted(iSumHandTwo);
+                Console.WriteLine(iBustP);
+
+                if (bDealerBust == true || bPlayerBust == true)
+                {
+                    // Metod som kollar player mot dealer om bust
+                }
+                else if (iBustD == 3)
+                {
+                    iCon = prg.Continue(iBustD, iBustP, iSumHandOne, iSumHandTwo);
+                    
+                    if (iCon == 1)
+                    {
+                        int iNumerFor = 3; 
+                        for (int i = iNumerFor; i < iNumerFor++; i++)
+                        {
+
+                            iArrDealerCards[i] = prg.AssignDealerCard(rDealer, iArrDealerCards[i]);   //fastnat!!!!!!!!!!!!
+
+
+                            if (iArrDealerCards[i] == 11)
+                            {
+                                Console.WriteLine("Knekt");
+                            }
+                            else if (iArrDealerCards[i] == 12)
+                            {
+                                Console.WriteLine("Dam");
+                            }
+                            else if (iArrDealerCards[i] == 13)
+                            {
+                                Console.WriteLine("Kung");
+                            }
+                            else if (iArrDealerCards[i] == 14)
+                            {
+                                Console.WriteLine("Ess");
+                            }
+                        }
+                    }
+                }
             }
         }
 
@@ -113,6 +159,13 @@ namespace BlackJack
 
         }
 
+        public int[] AddNewCard(Random rand, int[] arriHand)
+        {
+
+
+            return arriHand;
+        }
+
         public int AssignPlayerCard(Random randP, int iCardP) //slumpar fram ett random kort för player
         {
             iCardP = randP.Next(2, 14);
@@ -120,15 +173,15 @@ namespace BlackJack
         }
 
         int iSumOne = 0;
-        public int CalcDhand(int[] iInputSum) //summerar dealer eller player hand
+        public int CalcDhand(int[] iInputSum) //sumerar dealer eller player hand
         {
             for (int i = 0; i < 2; i++)
             {
-                if (iInputSum[i] > 10 && iInputSum[i] < 14)               //beräknar alla tal över tio som tio hfaha
+                if (iInputSum[i] > 10 && iInputSum[i] < 14)               //beräknar alla tal över tio och under 14 som tio 
                 {
                     iInputSum[i] = 10;
                 }
-                else if (iInputSum[i] == 14) //beräknar alla ess som 11
+                else if (iInputSum[i] == 14)                              //beräknar alla ess som 11
                 {
                     iSumOne = 11;
                 }
@@ -139,15 +192,15 @@ namespace BlackJack
             }
 
             int iSumTwo = 0; 
-        public int CalcPhand(int[] iInputSum) //summerar dealer eller player hand
+        public int CalcPhand(int[] iInputSum) //sumerar dealer eller player hand
         {
             for (int i = 0; i < 2; i++)
             {
-                if (iInputSum[i] > 10 && iInputSum[i] < 14)               //beräknar alla tal över tio som tio 
+                if (iInputSum[i] > 10 && iInputSum[i] < 14)               //beräknar alla tal över tio och under 14 som tio 
                 {
                     iInputSum[i] = 10;
                 }
-                else if (iInputSum[i] == 14) //beräknar alla ess som 11
+                else if (iInputSum[i] == 14)                              //beräknar alla ess som 11
                 {
                     iSumTwo = 11;
                 }
@@ -157,22 +210,95 @@ namespace BlackJack
             return iSumTwo; 
             }
 
-        public void Bust()
-        { 
-            if (iSumOne >= 22)
+        public bool IsBusted(int iInputHand)
+        {
+            if (iInputHand > 21)
             {
-                Console.WriteLine("You're Busted, End of the game");
+                return false;
+            }
+
+            else if (iInputHand == 21)
+            {
+                return false;
+            }
+
+            else
+            {
+                return true;
             }
         }
 
-        void PlayersNamn(string strName) //Namn på spelaren
+        void PlayersNamn(string strName) //Namn på playern
         {
             Console.WriteLine("Hello, " + strName + "\n---------------------");
-            
+        }
+
+        public void BustedOrNot (int iBustedD, int iBustedP, int SummanAvKortD, int SummanAvKortP) //Kollar om man är busted eller om man har black jack
+        {
+            if (iBustedD == 0)
+            {
+                Console.WriteLine("Player win ");
+            }
+            else if (iBustedP == 0)
+            {
+                Console.WriteLine("Dealer win!");
+            }
+            else if (iBustedD == 0 && iBustedP == 0)
+            {
+                Console.WriteLine("No one wins! It is a draw");
+            }
+            else if (iBustedD == 1 && SummanAvKortD > SummanAvKortP)
+            {
+                Console.WriteLine("Dealer Win!");
+            }
+            else if(iBustedD == 1 && SummanAvKortD < SummanAvKortP)
+            {
+                Console.WriteLine("Player Win!");
+            }
+            else if(iBustedD == 2)
+            {
+                Console.WriteLine("Black Jack, Dealer win");
+            }
+            else if (iBustedP == 2)
+            {
+                Console.WriteLine("Black Jack, Player win");
+            }
+        }
+
+        int iContinue = 1;
+        string Answer;
+        public int Continue(int iContinueD, int iContinueP, int SummanAvKortD, int SummanAvKortp)
+        { 
+            if(iContinueD == 3)
+            {
+                return iContinue;
+            }
+            else if(iContinueP == 3)
+            {
+                Console.WriteLine("Do you want a new card?");
+                Answer = Console.ReadLine();
+
+                if (Answer == "yes")
+                {
+                    iContinue = 2;
+                    return iContinue;
+                }
+                else if (Answer == "no")
+                {
+                    iContinue = 3;
+                    return iContinue;
+                }
+                else
+                {
+                    return iContinue;
+                }
+             }
+                else 
+                {
+                    return iContinue;
+                }
 
         }
-           
-        
     }             
           
        
