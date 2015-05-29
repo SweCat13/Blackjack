4f42cea5a9f3658dd8202cc57027faf61f5d1298
SweCat13 <claranermark@hotmail.com>
2015-05-29 10:25:02 +0200

    Klart för inlämning, men med några saker att fixa

--- BlackJack/BlackJack/BlackJack.cs
+++ BlackJack/BlackJack/BlackJack.cs
@@ -34,10 +34,8 @@ namespace BlackJack
             Console.WriteLine("Play Black Jack\n Start the game by dealing cards? (y/n)");
             string strStartGame = Console.ReadLine();
 
-            if (strStartGame == "y")                                //spel-loop
+            if(strStartGame == "y")                                //spelsats
             Console.Clear();
-
-
             {
                 for (int i = 0; i < 2 ; i++) // int i = 0; i < CardsOnHand; i++ if (arriHand[i] >= 11) {GetCardName(Hand[i]);}
                 {
@@ -65,30 +63,36 @@ namespace BlackJack
                 bPlayerBust = prg.IsBusted(iSumHandTwo);
                 Console.WriteLine(bPlayerBust);
 
-                while (!bDealerBust || !bPlayerBust)
+                while (!bDealerBust || !bPlayerBust) //while loop med krav att någon av playern och dealern inte är bust
                 {
-                    if (bDealerBust == true || bPlayerBust == true)
+                    if (bPlayerBust == true)  //bryter ut ur loopen om någon är bust
                     {
+                        Console.WriteLine("You loose");
+                        break;
+                    }
+                    else if (bDealerBust == true)
+                    {
+                        Console.WriteLine("You win!!");
                         break;
                     }
 
-                    else
+                    else if(bDealerBust == false && bPlayerBust == false)
                     {
-                        Console.WriteLine("Hit or stand? (1, 2)");
+                        Console.WriteLine("Hit or stand? (1, 2)");           //playern kan välja att fortsätta ta kort eller stanna
 
                         int iAnswer = 0;
                         iAnswer = int.Parse(Console.ReadLine());
 
-                        if (iAnswer == 1)
+                        if (iAnswer == 1)                                  //playern tar ett till kort och playerns hand summeras igen
                         {
                             prg.AddNewCard(rPlayer, iArrPlayerCards);
                             foreach (int i in iArrPlayerCards)
                             {
                                 for (int j = 0; j < iArrPlayerCards.Length; j++)
                                 {
-                                    if (iArrPlayerCards[j] >= 11) 
-                                    { 
-                                        prg.GetCardName(iArrPlayerCards[j]); 
+                                    if (iArrPlayerCards[j] >= 11)
+                                    {
+                                        prg.GetCardName(iArrPlayerCards[j]);
                                     }
                                 }
                                 Console.WriteLine(i);
@@ -97,15 +101,87 @@ namespace BlackJack
                             }
 
                             Console.WriteLine("Player sum: " + iSumHandTwo);
+                            bPlayerBust = prg.IsBusted(iSumHandTwo);              //kollar om playern är bust
+                            Console.WriteLine(bPlayerBust);
+                            
+                            if (iSumHandTwo == 21)                               //om playern får 21 vinner playern
+                            {
+                                bDealerBust = true;
+                            }
+
+
+                            if (iSumHandTwo > iSumHandOne && iSumHandTwo < 21)  //Dealern tar ett till kort om den har lägre kort än playern och playern inte är bust
+                            {
+                                prg.AddNewCard(rPlayer, iArrDealerCards);
+                                foreach (int i in iArrDealerCards)
+                                {
+                                    for (int j = 0; j < iArrDealerCards.Length; j++)
+                                    {
+                                        if (iArrDealerCards[j] >= 11)
+                                        {
+                                            prg.GetCardName(iArrDealerCards[j]);
+                                        }
+                                    }
+                                    Console.WriteLine(i);
+                                    iSumHandOne = 0;
+                                    iSumHandOne = prg.CalcHand(iArrDealerCards);
+                                }
+
+                                Console.WriteLine("Dealer sum: " + iSumHandOne);
+                                bDealerBust = prg.IsBusted(iSumHandOne);          //kollar om dealern är bust
+                                Console.WriteLine(bDealerBust);
+
+                                if (iSumHandOne == 21)
+                                {
+                                    bPlayerBust = true;
+                                }
+                            }
                         }
 
-                        else
+                        else                                  //om playern väljer stand
                         {
                             Console.WriteLine("Nope");
-                            break;
+
+
+                            if (iSumHandOne < iSumHandTwo) //dealern ska ta ett till kort om den har lägre summa än playern
+                            {
+                                while (iSumHandOne < iSumHandTwo)
+                                {
+                                    prg.AddNewCard(rPlayer, iArrDealerCards);
+                                    foreach (int i in iArrDealerCards)
+                                    {
+                                        for (int j = 0; j < iArrDealerCards.Length; j++)
+                                        {
+                                            if (iArrDealerCards[j] >= 11)
+                                            {
+                                                prg.GetCardName(iArrDealerCards[j]);
+                                            }
+                                        }
+                                        Console.WriteLine(i);
+                                        iSumHandOne = 0;
+                                        iSumHandOne = prg.CalcHand(iArrDealerCards);
+                                    }
+
+                                    bDealerBust = prg.IsBusted(iSumHandOne);  //kollar om dealern är bust
+                                    Console.WriteLine(bDealerBust);
+
+                                    Console.WriteLine("Dealer sum: " + iSumHandOne);
+                                }
+
+                                if (iSumHandOne > iSumHandTwo || iSumHandOne == iSumHandTwo)        //om ingen är bust vinner den med högst hand eller om båda har lika så vinner dealern
+                                {
+                                    bPlayerBust = true;
+                                }
+
+                            }
+                            else                                                      //om dealern har högre kort är playern vinner dealern
+                            {
+                                bPlayerBust = true; 
+                            }
                         }
 
                     }
+
                 }
 
                 
@@ -155,18 +231,19 @@ namespace BlackJack
         public int CalcHand(int[] iInputSum) //sumerar dealer eller player hand
         {
             iSumOne = 0;
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 10; i++)
             {
                 if (iInputSum[i] > 10 && iInputSum[i] < 14)               //beräknar alla tal över tio och under 14 som tio 
                 {
                     iInputSum[i] = 10;
                 }
-                else if (iInputSum[i] == 14)                              //beräknar alla ess som 11
-                {
-                    iSumOne = 11;
-                }
 
                 iSumOne = iSumOne + iInputSum[i];
+
+                if (iSumOne > 21 && iInputSum[i] == 11)                  
+                {
+                    iSumOne = iSumOne - 10; 
+                }
             }
             return iSumOne;
  
@@ -203,19 +280,19 @@ namespace BlackJack
            {
                case 11:
                  return "Knekt";
-                 break;
+                 
 
                case 12:
                 return "Dam";
-                break;
+                
 
                case 13:
                 return "Kung";
-                break;
+                
 
                case 14:
                 return "Ess";
-                break;
+                
                             
            }
 
